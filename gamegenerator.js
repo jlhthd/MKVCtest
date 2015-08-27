ce6e46a259ad44d9e3ff154a44c73b9e3c4530ee
jlhthd <jlhthd@mail.missouri.edu>
2015-08-27 13:00:21 -0500

    added javascript generation

---
+++ gamegenerator.js
@@ -0,0 +1,28 @@
+var Card = document.registerElement('card-el');
+
+function createCards() {
+    "use strict";
+    var house = document.createElement("div");
+    house.className = "house";
+    
+    var i = 0;
+    for (i = 0; i < 6; i++) {
+        var card = document.createElement("card-el");
+        
+        var cardHeader = document.createElement("h3");
+        var cardTitle = document.createTextNode("Card Name");
+        cardHeader.appendChild(cardTitle);
+        
+        var cardBody = document.createElement("p");
+        var cardText = document.createTextNode("Card Text");
+        cardBody.appendChild(cardText);
+        
+        card.appendChild(cardHeader);
+        card.appendChild(cardBody);
+        
+        house.appendChild(card);
+    }
+    
+    var el = document.getElementById("main");
+    el.appendChild(house);
+}
