3d776cfa710596573d40389591d836f7d868bd0e
jlhthd <jlhthd@mail.missouri.edu>
2015-09-01 13:22:28 -0500

    changing to inline-block

--- gamegenerator.js
+++ gamegenerator.js
@@ -5,44 +5,44 @@ function createCards() {
     var board = document.createElement("div");
     board.className = "board";
     
-    var i = 0;
-    for (i = 0; i < 6; i++) {
-        //create housing
-        var cardHouse = document.createElement("div");
-        cardHouse.className = "cardhouse";
-        var cardOffset = i * 16.5;
-        cardHouse.style.left = cardOffset + "%";
-        
-        //create card
-        var card = document.createElement("card-el");
-        card.className = "bluecard";
-        
-        var cardHeader = document.createElement("h3");
-        var cardTitle = document.createTextNode("Card Name");
-        cardHeader.appendChild(cardTitle);
-        
-        var cardBody = document.createElement("p");
-        var cardText = document.createTextNode("Card Text");
-        cardBody.appendChild(cardText);
-        
-        card.appendChild(cardHeader);
-        card.appendChild(cardBody);
-        
-        //add card to housing
-        cardHouse.appendChild(card);
-        
-        //add num cards
-        var numCardsDiv = document.createElement("div");
-        numCardsDiv.id = "numCards" + i;
-        
-        var numCardsText = document.createTextNode("6");
-        numCardsDiv.appendChild(numCardsText);
-        
-        cardHouse.appendChild(numCardsDiv);
-        
-        //add housing to board
-        board.appendChild(cardHouse);
-    }
+    //create line
+    
+//    var i = 0;
+//    for (i = 0; i < 6; i++) {
+//        //create housing
+//        var cardHouse = document.createElement("div");
+//        cardHouse.className = "cardhouse";
+//        
+//        //create card
+//        var card = document.createElement("card-el");
+//        card.className = "bluecard";
+//        
+//        var cardHeader = document.createElement("h3");
+//        var cardTitle = document.createTextNode("Card Name");
+//        cardHeader.appendChild(cardTitle);
+//        
+//        var cardBody = document.createElement("p");
+//        var cardText = document.createTextNode("Card Text");
+//        cardBody.appendChild(cardText);
+//        
+//        card.appendChild(cardHeader);
+//        card.appendChild(cardBody);
+//        
+//        //add card to housing
+//        cardHouse.appendChild(card);
+//        
+//        //add num cards
+//        var numCardsDiv = document.createElement("div");
+//        numCardsDiv.id = "numCards" + i;
+//        
+//        var numCardsText = document.createTextNode("6");
+//        numCardsDiv.appendChild(numCardsText);
+//        
+//        cardHouse.appendChild(numCardsDiv);
+//        
+//        //add housing to board
+//        board.appendChild(cardHouse);
+//    }
     
     var el = document.getElementById("main");
     el.appendChild(board);
